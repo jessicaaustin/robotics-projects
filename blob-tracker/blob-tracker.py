5111217505d0c5734e3baa64c0c0dc6d3ad0a461
Jessica Austin <livermouse@gmail.com>
2011-11-26 09:22:38 -0600

    blob tracker finds blue ball in image and indicates where it is

---
+++ blob-tracker/blob-tracker.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+#
+# Tracks a blue ball using OpenCV
+#
+
+import cv
+
+# the index of your video feed
+# if /dev/videoN is your camera device, then MY_CAMERA = N
+MY_CAMERA = 1
+
+# convert the given image to a binary image where all values are 
+# zero other than areas with blue hue
+def thresholded_image(image):
+    # convert image to hsv
+    image_hsv = cv.CreateImage(cv.GetSize(image), image.depth, 3)
+    cv.CvtColor(image, image_hsv, cv.CV_BGR2HSV)
+    # threshold the image
+    image_threshed = cv.CreateImage(cv.GetSize(image), image.depth, 1)
+    blue_min = cv.Scalar(70, 110, 120)
+    blue_max = cv.Scalar(105, 255, 255)
+    cv.InRangeS(image_hsv, blue_min, blue_max, image_threshed)
+    return image_threshed
+
+# initialize camera feed
+capture = cv.CaptureFromCAM(MY_CAMERA)
+if not capture:
+    print "Could not initialize camera feed!"
+    exit(1)
+
+# create display windows
+cv.NamedWindow('camera', cv.CV_WINDOW_AUTOSIZE)
+cv.NamedWindow('threshed', cv.CV_WINDOW_AUTOSIZE)
+
+pos_x,pos_y=0,0
+
+# read from the camera
+print "Tracking ball... press any key to quit"
+while 1:    
+    image = cv.QueryFrame(capture)
+    if not image:
+        break
+
+    # get the thresholded image
+    image_threshed = thresholded_image(image)
+
+    # finds the contours in our binary image
+    contours = cv.FindContours(cv.CloneImage(image_threshed), cv.CreateMemStorage())
+    # if there is a ball in the frame
+    if len(contours) != 0:
+        # calculate the moments to estimate the position of the ball
+        moments = cv.Moments(contours, 1)
+        moment10 = cv.GetSpatialMoment(moments, 1, 0)
+        moment01 = cv.GetSpatialMoment(moments, 0, 1)
+        area = cv.GetCentralMoment(moments, 0, 0)
+
+        # if we got a good enough blob
+        if area>0:
+            pos_x = moment10/area
+            pos_y = moment01/area
+
+            print("pos=(%s,%s)"%(pos_x,pos_y))
+
+    # show where the ball is located
+    ball_indicator = cv.CreateImage(cv.GetSize(image), image.depth, 3)
+    cv.Circle(ball_indicator, (int(pos_x),int(pos_y)), 8, (0,255,0), 2)
+    cv.Add(image, ball_indicator, image)
+
+    # show the images
+    cv.ShowImage('threshed', image_threshed)
+    cv.ShowImage('camera', image)
+
+    # break from the loop if there is a key press
+    c = cv.WaitKey(10)
+    if c != -1:
+        break
+
