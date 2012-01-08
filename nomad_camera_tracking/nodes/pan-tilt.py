a3e32e8779b4a9e33b7045ba337772f556b8679f
Bill Mania <maniabill@yahoo.com>
2012-01-08 12:30:06 -0600

    Basic empty framework for a ROS Subscriber node

---
+++ nomad_camera_tracking/nodes/pan-tilt.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+pan-tilt.py
+
+subscribes to the source of color blob location coordinates
+and updates the aim of the camera, based on those coordinates.
+"""
+
+import roslib ; roslib.load_manifest('nomad_camera_tracking')
+import rospy
+
+from geometry_msgs.msg import Point
+
+def updateBlobLocation(coordinateUpdateMessage):
+	# adjust the camera
+	print Point
+
+	return
+
+def mainControlLoop():
+
+    blobCoordSubscriber = rospy.Subscriber('blob_coord', Point, updateBlobLocation)
+
+	while not rospy.is_shutdown():
+		rospy.spin()
+
+	return
+
+if __name__ == '__main__':
+
+    rospy.init_node('pan-tilt', log_level = rospy.DEBUG)
+    rospy.loginfo('pan-tilt starting')
+
+    mainControlLoop()
+
+    rospy.logwarn('Stopping')
+
+    sys.exit(0)
+
